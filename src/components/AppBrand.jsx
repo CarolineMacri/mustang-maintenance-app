b608cd5a8719d3bdce7a1715d615705d363e69d4
Caroline Macri <fishknit1@gmail.com>
2026-04-03 21:01:42 -0400

    Change  to stripes in app brand component

--- src/components/AppBrand.jsx
+++ src/components/AppBrand.jsx
@@ -1,14 +1,14 @@
-// src/components/AppBrand.jsx
+// src/components/AppBrand.jsx//
 
 import styles from './AppBrand.module.css';
 
 export default function AppBrand() {
   return (
     <div className={styles.brand}>
-      <div className={styles.iconWrap} aria-hidden="true">
-        <span className={styles.icon} role="img" aria-label="horse">
-          🐎
-        </span>
+      <div className={styles.stripeMark} aria-hidden="true">
+        <span className={`${styles.stripe} ${styles.stripeBold}`}></span>
+        <span className={`${styles.stripe} ${styles.stripeMedium}`}></span>
+        <span className={`${styles.stripe} ${styles.stripeThin}`}></span>
       </div>
 
       <div className={styles.textBlock}>
