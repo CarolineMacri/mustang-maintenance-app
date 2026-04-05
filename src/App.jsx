8fdcc8620ee674b2a40420dda36d7dbdb3d252f4
Caroline Macri <fishknit1@gmail.com>
2026-04-05 11:29:35 -0400

    feat: import initial data and setstate for assets and maintenance records

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import './styles/global.css';
-import './styles/utilities.css';
+import { useState } from 'react';
+
+import initialMaintenance from './data/maintenance.json';
+import initialAssets from './data/assets.json';
 
 import AppLayout from './layout/AppLayout';
 
+import './styles/global.css';
+import './styles/utilities.css';
+
 function App() {
+  const [assets, setAssets] = useState(initialAssets);
+  const [maintenance, setMaintenance] = useState(initialMaintenance);
+  console.log(assets);
+  console.log(maintenance);
+
   return <AppLayout />;
 }
 
